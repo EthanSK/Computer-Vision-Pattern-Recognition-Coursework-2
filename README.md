bb9a8a51b034b0d76aea8f90920c3b757bf89fcf
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2021-03-11 16:17:17 +0000

    new shitty structure

--- README.md
+++
@@ -1,3 +0,0 @@
-# Computer-Vision-Pattern-Recognition-Coursework-2
-
-Run setup.m before running any other scripts so it adds the path to the workspace correctly.
--- utils/resave.m
+++
@@ -1,8 +0,0 @@
-function [] = resave(matfilePath)
-% resaves a .mat file so that it is in the correct format for partial loading
-
-clear global;
-load(matfilePath);
-save(matfilePath, '-v7.3');
-
-end
