5b6313f717de52a1d7b591e5acc1bcf85e0d9663
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2021-03-12 19:26:35 +0000

    hot lda

--- sectionC/task2.m
+++ sectionC/task2.m
@@ -38,11 +38,45 @@ zlim([-3, 3]);
 patch('Faces', [1 3 2 4], 'Vertices', [verts(:, 1)'; -verts(:, 1)'; verts(:, 2)'; -verts(:, 2)'], 'FaceAlpha', 0.5, 'FaceColor', 'red');
 
 %plot 2d representation
+
 featureVec2d = eigVecs(:, 1:2); 
-projected2d = PVTStandardized * featureVec2d;
+projected2d = PVTStandardized * featureVec2d; % 20x2
+
+projectedMeans2dA = [
+    mean(projected2d(1:10, 1)); mean(projected2d(1:10, 2));
+];
+projectedMeans2dB = [
+    mean(projected2d(11:20, 1)); mean(projected2d(11:20, 2));
+];
 
 plotData(names(objectIdxs(1)), names(objectIdxs(2)), @(i, startIdx, endIdx) scatter(projected2d(startIdx:endIdx, 1), projected2d(startIdx:endIdx, 2), 'MarkerFaceColor', colors(i, :)));
+
+plot(projectedMeans2dA(1), projectedMeans2dA(2),  'o', 'MarkerFaceColor', colors(1, :), 'MarkerSize', 15)
+plot(projectedMeans2dB(1), projectedMeans2dB(2),  'o', 'MarkerFaceColor', colors(2, :), 'MarkerSize', 15)
+
 grid on;
 
 xlabel('LD1');
 ylabel('LD2');
+
+
+%plot 1d representation
+
+featureVec1d = eigVecs(:, 1); 
+projected1d = PVTStandardized * featureVec1d;
+
+projectedMeans1dA = [
+    mean(projected1d(1:10, 1))
+];
+projectedMeans1dB = [
+    mean(projected1d(11:20, 1))
+];
+
+plotData(names(objectIdxs(1)), names(objectIdxs(2)), @(i, startIdx, endIdx) scatter(projected1d(startIdx:endIdx, 1), ones(1, 10), 'MarkerFaceColor', colors(i, :)));
+
+plot(projectedMeans1dA(1), 1, 'o', 'MarkerEdgeColor', colors(1, :), 'MarkerSize', 15)
+plot(projectedMeans1dB(1), 1, 'o', 'MarkerEdgeColor', colors(2, :), 'MarkerSize', 15)
+
+grid on;
+
+xlabel('LD1');
