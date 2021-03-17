4e35459b8b8b58d277d45198d99a759adbbf87a5
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2021-03-17 16:25:46 +0000

    fixt

--- sectionC/task1d.m
+++ sectionC/task1d.m
@@ -1,8 +1,8 @@
 objectIdxs = [4 6]; % Flour Sack & Steel Vase
 
-plotLDA2d(objectIdxs, [1 2],  'task1c', 'PV'); % PV
-plotLDA2d(objectIdxs, [1 3],  'task1c', 'PT'); % PT
-plotLDA2d(objectIdxs, [2 3], 'task1c', 'TV'); % TV
+plotLDA2d(objectIdxs, [1 2], 'task1d', 'PV'); % PV
+plotLDA2d(objectIdxs, [1 3], 'task1d', 'PT'); % PT
+plotLDA2d(objectIdxs, [2 3], 'task1d', 'TV'); % TV
  
-plotLDA3d(objectIdxs, [1 2 3], 'task1c', 'PVT'); % PVT
+plotLDA3d(objectIdxs, [1 2 3], 'task1d', 'PVT'); % PVT
  
